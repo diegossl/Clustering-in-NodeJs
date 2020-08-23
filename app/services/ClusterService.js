92495f7952d2b560db6f9a8c2c5a1b5647c66865
Diego <diegossl94@gmail.com>
2020-08-22 23:04:03 -0300

    Cluster changed

--- app/services/ClusterService.js
+++ app/services/ClusterService.js
@@ -2,22 +2,29 @@ const cluster  = require('cluster')
 const { cpus } = require('os')
 
 const isMaster   = cluster.isMaster
-const numWorkers = cpus().length
+const numCores = cpus().length
 
 if (isMaster) {
-  console.log(`Forking ${numWorkers} workers`)
+  console.log(`Master cluster setting up ${numCores} workers`)
 
+  let workers = []
   for (let index = 0; index < numWorkers; index++) {
-    cluster.fork()
+    workers.push(cluster.fork())
+    workers[index].on('message', (message) => {
+      console.log(message)
+    })
   }
 
   cluster.on('online', (worker) => {
     console.log(`Worker ${worker.process.pid} is online`)
   })
   
-  cluster.on('exit', (worker, exitCode) => {
-    console.log(`Worker ${worker.process.id} exited with code ${exitCode}`)
-    console.log(`Starting a new worker`)
-    cluster.fork()
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`)
+    console.log('Starting a new worker')
+    workers.push(cluster.fork())
+    workers[workers.length-1].on('message', (message) => {
+      console.log(message)
+    })
   })
 }
